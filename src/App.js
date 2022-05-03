06089cfb89300cf8645d15ab1f2e70e53d43f732
U0L9510 <massimo.carta@esternibisp.com>
2022-05-03 18:15:29 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+let variable = [];
+
+export const options = {
+  responsive: true,
+  interaction: {
+    mode: 'index',
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart - Multi Axis',
+    },
+  },
+  scales: {
+    y: {
+      type: 'linear',
+      display: true,
+      position: 'left',
+    },
+    y1: {
+      type: 'linear',
+      display: true,
+      position: 'right',
+      grid: {
+        drawOnChartArea: false,
+      },
+    },
+  },
+};
+
+const labels = ['December', 'January', 'February', 'March'];
+const monthsTotals = [
+  { titoliLunghi: 79.272, htc: 28.104, htcs: 26.933, htcsIspLux: 1.364, otherHtcHtcs: 1.095, tradingLong: 20.371, tradingShort: -16.559 },
+  { titoliLunghi: 82.993, htc: 30.119, htcs: 28.599, htcsIspLux: 1.540, otherHtcHtcs: 1.132, tradingLong: 20.217, tradingShort: -20.195 },
+  { titoliLunghi: 81.915, htc: 29.950, htcs: 27.862, htcsIspLux: 1.387, otherHtcHtcs: 1.387, tradingLong: 20.131, tradingShort: -18.512 },
+  { titoliLunghi: 80.240, htc: 30.737, htcs: 26.418, htcsIspLux: 1.150, otherHtcHtcs: 1.228, tradingLong: 19.274, tradingShort: -17.378 }
+];
+const monthsTotalsStoric = [
+  { titoliLunghi: 78.272, htc: 28.104, htcs: 26.933, htcsIspLux: 1.364, otherHtcHtcs: 1.095, tradingLong: 20.371, tradingShort: -16.559 },
+  { titoliLunghi: 81.993, htc: 30.119, htcs: 28.599, htcsIspLux: 1.540, otherHtcHtcs: 1.132, tradingLong: 20.217, tradingShort: -20.195 },
+  { titoliLunghi: 82.915, htc: 29.950, htcs: 27.862, htcsIspLux: 1.387, otherHtcHtcs: 1.387, tradingLong: 20.131, tradingShort: -18.512 },
+  { titoliLunghi: 81.240, htc: 30.737, htcs: 26.418, htcsIspLux: 1.150, otherHtcHtcs: 1.228, tradingLong: 19.274, tradingShort: -17.378 }
+];
+
+ 
+
+export function App(props) {
+  let data = {
+    labels,
+    datasets: [
+      {
+        label: props.prop[0].titolo,
+        data: props.prop[2].vars.map((x) => x),
+        borderColor: 'rgb(' + props.prop[1].color +')',
+        backgroundColor: 'rgba('+ props.prop[1].color  + ', 0.5)',
+        yAxisID: 'y',
+      },
+    ],
+  };
+  console.log(props.prop)
+  return <Line options={options} data={data} />
+      
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { App } from './App';
+import { MyFunc } from './MyFunc';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById('root');
+let goviesItalia = [{ titolo: 'Govies Italia' }, { color: '255, 99, 132' }, { vars: [10690, 11670, 11219, 11616] }];
+let goviesOther = [{ titolo: 'Govies Other' }, { color: '54, 162, 235' }, { vars: [7368, 8288, 8237, 8261] }];
+let emergingMarkets = [{ titolo: 'Emerging Markets' }, { color: '255, 206, 86' }, { vars: [1670, 1741, 1885, 2092] }];
+let corporate = [{ titolo: 'Corporate' }, { color: '75, 192, 192' }, { vars: [4918, 4900, 5030, 4965] }];
+let financial = [{ titolo: 'Financial' }, { color: '153, 102, 255' }, { vars: [2936, 3000, 3004, 2872] }];
+let securitizations = [{ titolo: 'Securitizations' }, { color: '255, 159, 64' }, { vars: [522, 520, 575, 571] }];
+
+let vars = [11616, 8621, 2092, 4965, 2872, 571 ] ;
+let totHTC = 0;
+for(let i = 0; i<vars.length; i++){
+    totHTC += vars[i];
+}
+createRoot(rootElement).render(
+    <Container fluid="md">
+        <Row>
+        <Col xs={1}></Col>
+            <Col xs={6}>
+                <h1>Totale HTC : {totHTC}</h1>
+                <MyFunc prop={vars}/>
+            </Col>
+            <Col xs={1}></Col>
+            <Col xs={4}>
+            <br></br>
+            <br></br>
+                <Row>
+                    <App prop={goviesItalia} />
+                    <App prop={goviesOther} />
+                    <App prop={emergingMarkets} />
+                </Row>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={4}>
+                <App prop={corporate} />
+            </Col>
+            <Col xs={4}>
+                <App prop={financial} />
+            </Col>
+            <Col xs={4}>
+                <App prop={securitizations} />
+            </Col>
+        </Row>
+    </Container>);
